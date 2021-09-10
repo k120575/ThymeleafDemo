c0c2029888ae469ab2eff7343e41b8341fff6c6c
Kevin <kevin.liao@ambernet.com.tw>
2021-09-10 14:12:14 +0800

    修改pom

--- src/main/java/com/thymeleafdemo/controller/IndexController.java
+++ src/main/java/com/thymeleafdemo/controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @GetMapping("/hello")
+    @GetMapping("/Hello")
     public String hello(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
             Model model) {
